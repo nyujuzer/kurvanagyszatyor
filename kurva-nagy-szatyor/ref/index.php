2a4260bee83c5779d5ce6cddb545194b8164f3e5
nyujuzer <107214326+nyujuzer@users.noreply.github.com>
2022-11-01 22:03:01 +0100

    Add files via upload

---
+++ kurva-nagy-szatyor/ref/index.php
@@ -0,0 +1,40 @@
+<?php
+$logo = "logo";
+$ext = "png";
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="werkyawanke.css">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>KURVA NAGY SZATYOR</title>
+</head>
+<body>
+<div id="box1">
+	<div id="nav">
+		<img id="logo"src='logo.png' style='height: 65px; width: 65px;'>
+		<h1>KURVA NAGY <br>SZATYOR &trade;</h1>
+	</div>
+	<div id="text">
+	<h2 class="big_boy"><br><br><br>MI IS AZ A KURVA NAGY SZATYOR?</h2>
+	<p><br>Pontosan az amire neked szükséged van, baszki! Egy szatyor tele azzal a faszsággal, ami neked kell! Kell egy nagy szatyor eljegyzési gyűrű? A tied! kell egy nagy szatyor kávébab? Megkapod. <br> Ember, Ha az kell, mi eladunk neked egy kurva nagy szatyor levegőt is</p>
+	<h2 class="big_boy">Mégis mekkora egy ilyen szatyor?</h2>
+	<p><br><br><br>Mi az, hogy mekkora. Rohadtul nagy. Képzeld el a Tesco-s Nagy zöld táskát, és képzeld el, hogy annál is nagyobb</p><br><br><br><span class="size"> Még nagyobb</span></p>
+	</div>
+	<div id="pics">
+		<img class="imag" id="purchase"class="image" src="purchase.png">		
+		<img class="imag" id="about-us"class="image" src="about-us.png">
+		<img class="imag" id="RnD" class="image" src="RnD-dev-png">
+		<div id="click">
+		<a calss="lnk" href="vasarlas.php">Vásárlás</a>
+		<a class="lnk" href="about-us.php">végünkről</a>
+		<a class="lnk" href="RnD-dev.php">Kutatásaink</a>
+		</div>
+	</div>
+
+</div>
+</body>
+</html>
+
---
+++ kurva-nagy-szatyor/ref/indexul-li.php
@@ -0,0 +1,33 @@
+<?php
+$logo = "logo";
+$ext = "png";
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="werkyawanke.css">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>KURVA NAGY SZATYOR</title>
+</head>
+<body>
+<div id="box1">
+	<?php echo "<img id='logo'style='height:65px; width:75px;'src='logo/$logo.$ext'>";?>
+	<h1 id="nav">KURVA NAGY SZATYOR &trade;</h1>
+	<div id="text">
+	<h1 class="big_boy"><br><br><br>MI IS AZ A KURVA NAGY SZATYOR?</h1>
+	<p>Pontosan az amire neked szükséged van, baszki! Egy szatyor tele azzal a faszsággal, ami neked kell!</p>
+	</div>
+	<div id="pics">
+		<ul>
+			<li><img id="purchase"class="image" src="purchase.png"></li>
+			<li><img id="about-us"class="image" src="about-us.png"></li>
+			<li><img id="RnD" class="image" src="RnD-dev-png"></li>
+		</ul>
+	</div>
+
+</div>
+</body>
+</html>
+
